f0120c6d881f5174801610119e57cb101a1b932c
Run Jin <sunnyking326@gmail.com>
2021-08-13 14:03:54 -0400

    rerun with 0 and 1 change

--- molecular_target_radiation/run_analysis.sh
+++ molecular_target_radiation/run_analysis.sh
@@ -10,9 +10,9 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-Rscript -e "rmarkdown::render('01-lgg_harmonized_diagnosis.Rmd', clean = TRUE)"
-
-Rscript -e "rmarkdown::render('02-lgg_cns_gtex.Rmd', clean = TRUE)"
+# Rscript -e "rmarkdown::render('01-lgg_harmonized_diagnosis.Rmd', clean = TRUE)"
+# 
+# Rscript -e "rmarkdown::render('02-lgg_cns_gtex.Rmd', clean = TRUE)"
 
 Rscript -e "rmarkdown::render('03-km_w_logrank_survival_lgg.Rmd', clean = TRUE)"
 

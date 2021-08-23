fe36e558062aa89f49960efc02ce17b2ae1b930f
Run Jin <sunnyking326@gmail.com>
2021-08-23 17:58:41 -0400

    uncomment codes

--- molecular_target_radiation/run_analysis.sh
+++ molecular_target_radiation/run_analysis.sh
@@ -10,14 +10,14 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-# Rscript -e "rmarkdown::render('01-TPM_vs_harmonized_diagnosis.Rmd', clean = TRUE)"
-# 
-# Rscript -e "rmarkdown::render('02-lgg_cns_gtex.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('01-TPM_vs_harmonized_diagnosis.Rmd', clean = TRUE)"
+ 
+Rscript -e "rmarkdown::render('02-lgg_cns_gtex.Rmd', clean = TRUE)"
 
-# Rscript -e "rmarkdown::render('03a-km_w_logrank_survival_lgg.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('03a-km_w_logrank_survival_lgg.Rmd', clean = TRUE)"
 Rscript -e "rmarkdown::render('03b-km_w_logrank_survival_hgg.Rmd', clean = TRUE)"
 Rscript -e "rmarkdown::render('03c-km_w_logrank_survival_medullo.Rmd', clean = TRUE)"
-# 
-# Rscript -e "rmarkdown::render('04a-cox_reg_survival_lgg.Rmd', clean = TRUE)"
+ 
+Rscript -e "rmarkdown::render('04a-cox_reg_survival_lgg.Rmd', clean = TRUE)"
 Rscript -e "rmarkdown::render('04b-cox_reg_survival_hgg.Rmd', clean = TRUE)"
 Rscript -e "rmarkdown::render('04c-cox_reg_survival_medullo.Rmd', clean = TRUE)"

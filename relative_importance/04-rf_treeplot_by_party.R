23edb0aefcdee6c66ac122b3784d6666f9b26445
runjin326 <47674661+runjin326@users.noreply.github.com>
2022-02-01 11:40:56 -0500

    grid search added

--- relative_importance/04-rf_treeplot_by_party.R
+++ relative_importance/04-rf_treeplot_by_party.R
@@ -25,6 +25,11 @@ if(!dir.exists(plots_dir)){
   dir.create(plots_dir, recursive=TRUE)
 }
 
+results_dir <- file.path(analysis_dir, "results", "rf_party")
+if(!dir.exists(plots_dir)){
+  dir.create(plots_dir, recursive=TRUE)
+}
+
 meta_exp_input_dir <- opt$path_to_meta
 
 # run the tree generation function for all cancer groups 
@@ -46,9 +51,10 @@ for (i in 1:length(cg_list)){
                                 sep = "")
     # make the combined data compatible with the analysis 
     combined_data_sub <- meta_exp_combined %>% 
-      dplyr::select(-c("Kids_First_Biospecimen_ID", "Kids_First_Participant_ID", 
+      dplyr::select(-c("Kids_First_Participant_ID", 
                        "OS_status_recode", "OS_days", "PFS_status")) %>% 
-      dplyr::filter(!is.na(PFS_days))
+      dplyr::filter(!is.na(PFS_days)) %>%
+      tibble::column_to_rownames("Kids_First_Biospecimen_ID")
   
   } else{
     # piece together a model
@@ -57,19 +63,82 @@ for (i in 1:length(cg_list)){
                                 sep = "")
     # make the combined data compatible with the analysis 
     combined_data_sub <- meta_exp_combined %>% 
-      dplyr::select(-c("Kids_First_Biospecimen_ID", "Kids_First_Participant_ID", 
+      dplyr::select(-c("Kids_First_Participant_ID", 
                        "PFS_status_recode", "PFS_days", "OS_status")) %>% 
-      dplyr::filter(!is.na(OS_days))
+      dplyr::filter(!is.na(OS_days)) %>%
+      tibble::column_to_rownames("Kids_First_Biospecimen_ID")
   }
-    
+  
+  # run the grid search of mtry and ntree and find the optimal setting
+  k=1
+  cforest_tune_df <- data.frame(matrix(ncol = 4, nrow = 0))
+  colnames(cforest_tune_df) <- c("c_index", "mtry", "ntree", "mincriterion")
+  
+  # find the best parameters for cforest
+  for(m in 1:length(gene_variables)){
+    for(n in seq(from = 100, to = 2000, by = 100)){
+      for(j in seq(from = 0.5, to = 1, by = 0.05)){
+        
+        # define the control object using hyperparameter
+        cforest_ctrl_obj <- cforest_control(teststat = "max",
+                                            testtype = "Teststatistic",
+                                            mincriterion = j,
+                                            savesplitstats = FALSE,
+                                            ntree = n, 
+                                            mtry = m, 
+                                            replace = TRUE,
+                                            trace = FALSE)
+        
+        # run the cforest fit with the hyperparameters
+        cforest_fit <- cforest(as.formula(survival.formula), 
+                               data = as.data.frame(combined_data_sub), 
+                               subset = NULL, 
+                               weights = NULL,
+                               controls = cforest_ctrl_obj,
+                               xtrafo = ptrafo, 
+                               ytrafo = ptrafo, 
+                               scores = NULL)
+        
+        # run cindex
+        if(x == "LGG"){
+          harrelC1 <- rcorr.cens(predict(cforest_fit),
+                                 with(combined_data_sub,
+                                      Surv(PFS_days,PFS_status_recode)))
+        } else {
+          harrelC1 <- rcorr.cens(predict(cforest_fit),
+                                 with(combined_data_sub,
+                                      Surv(OS_days,OS_status_recode)))
+        }
+        
+        # store the results
+        cforest_tune_df[k,1] <- harrelC1[["C Index"]] %>% as.numeric()
+        cforest_tune_df[k,2] <- m
+        cforest_tune_df[k,3] <- n
+        cforest_tune_df[k,4] <- j
+        k <- k+1
+      }
+    }
+  }
+  
+  # write out the results 
+  cforest_tune_df %>% 
+    dplyr::arrange(c_index) %>%
+    readr::write_tsv(file.path(results_dir, paste0("rfparty_hyperparam_grid_results_for_", x, ".tsv")))
+  
+  # find the optimal parameter
+  # select the mtry, ntree and mincriterion value with the highest c index
+  cindex_max <- min(cforest_tune_df$c_index)
+  mtry_max<- cforest_tune_df[cforest_tune_df$c_index == cindex_max, "mtry"]
+  mincri_max<- cforest_tune_df[cforest_tune_df$c_index == cindex_max, "nodesize"]
+  
   ####### run the analysis with optimal parameter
   ctree_ctrl_obj <- ctree_control(teststat = "max",
                                   testtype = "Teststatistic",
-                                  mincriterion = 0.95,
+                                  mincriterion = c_index,
                                   minbucket = 5,
                                   stump = FALSE,
                                   maxsurrogate = 0,
-                                  mtry = 0, 
+                                  mtry = mtry_max, 
                                   savesplitstats = TRUE, 
                                   maxdepth = 0, 
                                   remove_weights = FALSE)

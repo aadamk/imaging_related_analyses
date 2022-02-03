ba42dc1ce010695f3430c90982c4c632f54bff22
runjin326 <47674661+runjin326@users.noreply.github.com>
2022-02-02 20:51:21 -0500

    fix run script

--- relative_importance/run_analysis.sh
+++ relative_importance/run_analysis.sh
@@ -18,31 +18,31 @@ expression_file="${data_dir}/gene-expression-rsem-tpm-collapsed.rds"
 short_long_match="${data_dir}/short_long_match.tsv"
 cancer_group_list="LGG,HGG,EPN,Medullo"
 
-# # Run CoxPH survival analysis for all genes of interest in all cancer group of interest
-# Rscript --vanilla 01-mboost_survival.R \
-# --histology $histology_file \
-# --expression $expression_file \
-# --cg_interest $cancer_group_list \
-# --short_long_match $short_long_match
-# 
-# # Calculate ssGSEA scores per cancer group of interest
-# Rscript --vanilla 02-ssgsea_analysis.R \
-# --histology $histology_file \
-# --expression $expression_file \
-# --cg_interest $cancer_group_list \
-# --short_long_match $short_long_match
-# 
-# # Run RF survival analysis for all genes of interest in all cancer group of interest
-# Rscript --vanilla 03-rfsrc_survival.R \
-# --histology $histology_file \
-# --expression $expression_file \
-# --cg_interest $cancer_group_list \
-# --short_long_match $short_long_match
-# 
-# # Output tree plot using party for all cancer group of interest
-# Rscript --vanilla 04-rf_treeplot_by_party.R \
-# --cg_interest $cancer_group_list \
-# --path_to_meta $meta_dir
+# Run CoxPH survival analysis for all genes of interest in all cancer group of interest
+Rscript --vanilla 01-mboost_survival.R \
+--histology $histology_file \
+--expression $expression_file \
+--cg_interest $cancer_group_list \
+--short_long_match $short_long_match
+
+# Calculate ssGSEA scores per cancer group of interest
+Rscript --vanilla 02-ssgsea_analysis.R \
+--histology $histology_file \
+--expression $expression_file \
+--cg_interest $cancer_group_list \
+--short_long_match $short_long_match
+
+# Run RF survival analysis for all genes of interest in all cancer group of interest
+Rscript --vanilla 03-rfsrc_survival.R \
+--histology $histology_file \
+--expression $expression_file \
+--cg_interest $cancer_group_list \
+--short_long_match $short_long_match
+
+# Output tree plot using party for all cancer group of interest
+Rscript --vanilla 04-rf_treeplot_by_party.R \
+--cg_interest $cancer_group_list \
+--path_to_meta $meta_dir
 
 # Output tree plot using party for all cancer group of interest
 Rscript --vanilla 05-model_compare.R \
